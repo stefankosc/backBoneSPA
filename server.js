0425586771116eab4765e85c101424a9538d46f5
ste <stefan.kosc@gmail.com>
2016-09-16 15:24:23 +0200

    added message route, and request handling for registration

--- server.js
+++ server.js
@@ -1,13 +1,68 @@
 const express = require('express');
-
 const app = express();
+const credentials = require('./credentials');
+const pg = require('pg');
+
 
 app.use(express.static(__dirname + '/static'));
+app.use(require('body-parser').urlencoded({
+    extended: false
+}));
+
+createNewPsqlClient = function (pgUser, pgPassword) {
+    var client = new pg.Client('postgres://' + credentials.pgUser + ':' + credentials.pgPassword + '@localhost:5432/users');
+    client.connect(function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    return client;
+}
 
 app.get('/', function (req, res) {
 
-
     res.sendFile(__dirname + '/index.html');
+});
+
+
+app.post('/registration', function(req, res) {
+
+    //cache.del('users');
+    if (!req.body.name.length || !req.body.email.length || !req.body.password.length) {
+        //res.redirect('/name/index.html');
+        return;
+    } else {
+        var client = createNewPsqlClient(credentials.pgUser, credentials.pgPassword);
+
+        var query = 'INSERT INTO messageBoardUsers(name, email, password) VALUES($1, $2, $3) RETURNING id';
+        var name = req.body.name;
+        var email = req.body.email;
+        var password = req.body.password;
+
+            client.query(query, [name, email, password], function(err, results) {
+                if (err) {
+                    console.log(err);
+
+                    var duplicateEmailError = ['Email exists in a database'];
+
+                } else {
+                    client.end();
+
+                    /*req.session.user = {
+                        data: req.body.firstname + ' ' + req.body.lastname,
+                        userID: results.rows[0].id,
+                        email: email
+                    }*/
+
+                    res.end();
+                }
+            });
+        //});
+    }
+});
+
+app.post('/login', function (req, res) {
+
 });
 
 
--- static/script.js
+++ static/script.js
@@ -10,34 +10,79 @@
 
     var Router = Backbone.Router.extend({
         routes: {
+            'startpage': 'startpage',
             'registration': 'registration',
-            'login': 'login'
+            'login': 'login',
+            'messages': 'messages'
+
         },
         login: function () {
+            $("#main").off();
             new LoginView({
                 model: new LoginModel(),
                 el: "#main"
             });
         },
         registration: function () {
+            $("#main").off();
             new RegistrationView({
                 model: new RegistrationModel(),
+                el: "#main"
+            });
+        },
+        startpage: function () {
+            $('#main').off();
+            new StartpageView({
+                model: new StartpageModel(),
+                el: "#main"
+            });
+        },
+        messages: function () {
+            $('#main').off();
+            new MessagesView({
+                model: new MessagesModel(),
                 el: "#main"
             });
         }
     });
 
-    var LoginModel = Backbone.Model.extend({
-        initialize: function () {
-        },
-        url: '/login'
-    });
 
-    var RegistrationModel = Backbone.Model.extend({
+
+
+    var StartpageModel = Backbone.Model.extend({
         initialize: function() {
         },
+        url: '/'
+    });
+
+    var StartpageView = Backbone.View.extend({
+        initialize: function() {
+
+            this.render();
+        },
+        render: function() {
+
+            this.$el.html(Handlebars.templates.startPage())
+        },
+        events: {
+            'click #loginButton': function() {
+                window.location.hash = 'login';
+            },
+            'click #registrationButton': function() {
+                window.location.hash = 'registration';
+            }
+        }
+    });
+
+
+
+
+    var RegistrationModel = Backbone.Model.extend({
+        save: function() {
+            return $.post(this.url, this.toJSON());
+        },
         url: '/registration'
-    })
+    });
 
     var RegistrationView = Backbone.View.extend({
         initialize: function() {
@@ -45,25 +90,71 @@
         },
         render: function() {
             this.$el.html(Handlebars.templates.registrationForm())
+        },
+        events: {
+            'click input[name="registrationInput"]': function(e) {
+                this.model.set(
+                    {
+                        'name': $('#registrationData').find('input[name="name"]').val(),
+                        'email': $('#registrationData').find('input[name="email"]').val(),
+                        'password': $('#registrationData').find('input[name="password"]').val()
+                    }
+
+                ).save().then(function() {
+                    console.log('saved');
+                    window.location.hash = 'messages';
+                })
+            }
         }
-    })
+    });
+
+
+
+
+    var LoginModel = Backbone.Model.extend({
+        initialize: function () {
+
+        },
+        url: '/login'
+    });
 
     var LoginView = Backbone.View.extend({
+
         initialize: function() {
             this.render();
-            console.log(this.model.id);
         },
         render: function() {
-
             this.$el.html(Handlebars.templates.loginForm())
         },
         events: {
-            'click #data': function(e) {
-                console.log('Helo');
+            'click input[type="button"][name="loginInput"]': function(e) {
+                this.model.save($('#loginData').find('input[name="email"]').val());
+                this.model.save($('#loginData').find('input[name="password"]').val());
             }
+        }
+    });
+
+
+
+
+    var MessagesModel = Backbone.Model.extend({
+
+        url: '/messages'
+    });
+
+    var MessagesView = Backbone.View.extend({
+        initialize: function() {
+            this.render();
         },
-    })
+        render: function() {
+            this.$el.html(Handlebars.templates.messagesBoard())
+        }
+    });
+
 
     var router = new Router();
+    console.log(Router);
     Backbone.history.start();
+    window.location.hash = 'startpage';
+
 }())
